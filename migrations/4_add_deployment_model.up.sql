e636ed0a89f11107e47d6aba669eccf9e50593dd
a.tselishchev <a.tselishchev@containerum.io>
2017-12-12 09:52:41 +0000

    Deployments in db.

---
+++ migrations/4_add_deployment_model.up.sql
@@ -0,0 +1,25 @@
+BEGIN TRANSACTION;
+CREATE TABLE deployments (
+	id uuid NOT NULL CONSTRAINT prim_key PRIMARY KEY,
+	ns_id uuid NOT NULL,
+	name varchar NOT NULL,
+	ram int NOT NULL,
+	cpu int NOT NULL,
+	create_time timestamp NOT NULL,
+	deleted boolean NOT NULL,
+	delete_time timestamp NOT NULL,
+
+	CONSTRAINT uniq_in_ns UNIQUE (ns_id, name),
+	CONSTRAINT valid_resources CHECK (ram > 0 AND cpu > 0)
+);
+CREATE TABLE deployment_volume (
+	depl_id uuid NOT NULL,
+	vol_id uuid NOT NULL,
+
+	CONSTRAINT uniq_pair UNIQUE(depl_id, vol_id)
+);
+CREATE TABLE containers (
+	depl_id uuid NOT NULL,
+	image varchar NOT NULL
+);
+COMMIT TRANSACTION;
