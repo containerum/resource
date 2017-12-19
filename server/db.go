31d942a518fdc23752ce86c264d888847f969f69
a.tselishchev <a.tselishchev@containerum.io>
2017-12-19 11:16:17 +0000

    Fix volume creation.

--- server/db.go
+++ server/db.go
@@ -484,12 +484,14 @@ func (db resourceSvcDB) volumeCreate(tariff model.VolumeTariff, user uuid.UUID, 
 			id,
 			capacity,
 			replicas,
-			tariff_id
-		) VALUES ($1,$2,$3,$4)`,
+			tariff_id,
+			is_persistent
+		) VALUES ($1,$2,$3,$4,$5)`,
 		volID,
 		tariff.StorageLimit,
 		tariff.ReplicasLimit,
 		tariff.TariffID,
+		tariff.IsPersistent,
 	)
 	if err != nil {
 		return
@@ -537,7 +539,8 @@ func (db resourceSvcDB) volumeList(user uuid.UUID) (vols []Volume, err error) {
 			a.access_level,
 			a.access_level_change_time,
 			v.capacity,
-			v.replicas
+			v.replicas,
+			v.is_persistent
 		FROM volumes v INNER JOIN accesses a ON a.resource_id=v.id
 		WHERE a.user_id=$1 AND a.kind='Volume'`,
 		user,
@@ -559,6 +562,7 @@ func (db resourceSvcDB) volumeList(user uuid.UUID) (vols []Volume, err error) {
 			&vol.AccessChangeTime,
 			&vol.Storage,
 			&vol.Replicas,
+			&vol.Persistent,
 		)
 		if err != nil {
 			return
