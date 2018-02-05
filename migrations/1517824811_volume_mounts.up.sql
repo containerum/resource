0a737555cad67b15c4ce645e75caca194e23cd54
Vladimir Stolyarov <xakep6666@gmail.com>
2018-02-05 13:13:08 +0300

    Fix misprint in foreign key definition

--- migrations/1517824811_volume_mounts.up.sql
+++ migrations/1517824811_volume_mounts.up.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS volume_mounts (
   mount_path TEXT NOT NULL,
   sub_path TEXT,
 
-  FOREIGN KEY (container_id) REFERENCES containters (id) ON DELETE CASCADE,
+  FOREIGN KEY (container_id) REFERENCES containers (id) ON DELETE CASCADE,
   FOREIGN KEY (volume_id) REFERENCES volumes (id) ON DELETE CASCADE,
   UNIQUE (container_id, mount_path)
 );
