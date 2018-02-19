a8424fe23822d1a4af996443c7701102d85edc95
Vladimir Stolyarov <xakep6666@gmail.com>
2018-02-19 14:15:53 +0300

    Update vendors

--- vendor/git.containerum.net/ch/json-types/resource-service/types.go
+++ vendor/git.containerum.net/ch/json-types/resource-service/types.go
@@ -72,11 +72,12 @@ func (v *Volume) Mask() {
 }
 
 type Storage struct {
-	ID       string `json:"id,omitempty" db:"id"`
-	Name     string `json:"name" db:"name"`
-	Used     int    `json:"used" db:"used"`
-	Size     int    `json:"size" db:"size"`
-	Replicas int    `json:"replicas"`
+	ID       string   `json:"id,omitempty" db:"id"`
+	Name     string   `json:"name" db:"name"`
+	Used     int      `json:"used" db:"used"`
+	Size     int      `json:"size" db:"size"`
+	Replicas int      `json:"replicas"`
+	IPs      []string `json:"ips" db:"ips"`
 }
 
 type Deployment struct {
