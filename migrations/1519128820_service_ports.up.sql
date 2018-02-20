c877151217e98d6dc9495c346ef0850645e8eb6c
Vladimir Stolyarov <xakep6666@gmail.com>
2018-02-20 15:38:05 +0300

    Add migrations for service ports model

---
+++ migrations/1519128820_service_ports.up.sql
@@ -0,0 +1,15 @@
+CREATE TYPE PROTOCOL AS ENUM (
+  'tcp',
+  'udp'
+);
+
+CREATE TABLE IF NOT EXISTS service_ports (
+  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  service_id UUID NOT NULL REFERENCES services (id) ON DELETE CASCADE,
+  name TEXT NOT NULL,
+  port INTEGER NOT NULL CHECK (port BETWEEN 1 AND 65535),
+  target_port INTEGER CHECK (target_port BETWEEN 1 AND 65535),
+  protocol PROTOCOL NOT NULL DEFAULT 'tcp'::PROTOCOL,
+
+  UNIQUE (service_id, name)
+);
