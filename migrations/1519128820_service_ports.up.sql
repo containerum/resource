827cc888918cdf5311e7fc5d4fb42ce0621d0b0d
Vladimir Stolyarov <xakep6666@gmail.com>
2018-02-20 15:58:11 +0300

    Fix check clause in target_port

--- migrations/1519128820_service_ports.up.sql
+++ migrations/1519128820_service_ports.up.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS service_ports (
   service_id UUID NOT NULL REFERENCES services (id) ON DELETE CASCADE,
   name TEXT NOT NULL,
   port INTEGER NOT NULL CHECK (port BETWEEN 1 AND 65535),
-  target_port INTEGER CHECK (target_port BETWEEN 1 AND 65535),
+  target_port INTEGER CHECK (target_port IS NULL OR target_port BETWEEN 1 AND 65535),
   protocol PROTOCOL NOT NULL DEFAULT 'tcp'::PROTOCOL,
 
   UNIQUE (service_id, name)
