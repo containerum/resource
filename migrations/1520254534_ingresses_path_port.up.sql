3b229c1415fd333c6319bafc19529d0ef19202e8
Vladimir Stolyarov <xakep6666@gmail.com>
2018-03-05 16:31:24 +0300

    Fix UPDATE query
    fix

--- migrations/1520254534_ingresses_path_port.up.sql
+++ migrations/1520254534_ingresses_path_port.up.sql
@@ -2,10 +2,10 @@ ALTER TABLE ingresses
   ADD COLUMN service_port INTEGER CHECK (service_port BETWEEN 1 AND 65535),
   ADD COLUMN path TEXT NOT NULL DEFAULT '/';
 
-UPDATE ingresses i
-SET i.service_port = (SELECT sp.port
+UPDATE ingresses
+SET service_port = (SELECT sp.port
                     FROM service_ports sp
-                    WHERE (sp.service_id,sp.protocol) = (i.service_id, 'tcp'::PROTOCOL)
+                    WHERE (sp.service_id,sp.protocol) = (ingresses.service_id, 'tcp'::PROTOCOL)
                     UNION ALL
                     SELECT NULL
                     FETCH FIRST 1 ROW ONLY);
