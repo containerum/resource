10766a04eaf149bbce69155b65ed529283236638
Vladimir Stolyarov <xakep6666@gmail.com>
2018-03-05 16:25:07 +0300

    Check service type in "check_service_port"

--- migrations/1520254534_ingresses_path_port.up.sql
+++ migrations/1520254534_ingresses_path_port.up.sql
@@ -16,12 +16,20 @@ ALTER TABLE ingresses
   ALTER COLUMN service_port SET NOT NULL;
 
 CREATE OR REPLACE FUNCTION check_service_port() RETURNS TRIGGER AS $check_service_port$
+DECLARE
+  s_type SERVICE_TYPE;
 BEGIN
+  SELECT s.type INTO s_type FROM services s WHERE id = NEW.service_id;
+  IF s_type != 'external'::SERVICE_TYPE THEN
+    RAISE EXCEPTION 'service % is not external', NEW.service_id;
+  END IF;
+
   IF NOT EXISTS(
     SELECT 1 FROM service_ports sp WHERE (sp.service_id, sp.protocol) = (NEW.service_id, 'TCP')
   ) THEN
     RAISE EXCEPTION 'TCP port % not found in service', NEW.service_port;
   END IF;
+
   RETURN NEW;
 END;
 $check_service_port$ LANGUAGE plpgsql;
