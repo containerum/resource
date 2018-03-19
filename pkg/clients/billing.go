05b47159b57b70cafc6e84b5d3e906a66e47d523
Vladimir Stolyarov <xakep6666@gmail.com>
2018-03-19 11:32:45 +0300

    Fix fake tariffs data

--- pkg/clients/billing.go
+++ pkg/clients/billing.go
@@ -34,8 +34,7 @@ type dummyBillingClient struct {
 var fakeNSData = `
 [
   {
-    "id": "3c9d98af-ef8c-4486-ba28-01d83bdd2ddd",
-    "tariff_id": "f3091cc9-6dc3-470e-ac54-84defe011111",
+    "id": "f3091cc9-6dc3-470e-ac54-84defe011111",
     "created_at": "2017-12-26T13:53:56Z",
     "cpu_limit": 500,
     "memory_limit": 512,
@@ -48,8 +47,7 @@ var fakeNSData = `
     "price": 0
   },
   {
-    "id": "2f7f294d-3f53-4b10-94e2-e7411570d9a7",
-    "tariff_id": "4563e8c1-fb41-416a-9798-e949a2616260",
+    "id": "4563e8c1-fb41-416a-9798-e949a2616260",
     "created_at": "2017-12-26T13:57:45Z",
     "cpu_limit": 900,
     "memory_limit": 1024,
@@ -67,8 +65,7 @@ var fakeNSData = `
 var fakeVolumeData = `
 [
   {
-    "id": "cc2ac926-1ead-4ee6-9218-ee64d92fca2a",
-    "tariff_id": "15348470-e98f-4da0-8d2e-8c65e15d6eeb",
+    "id": "15348470-e98f-4da0-8d2e-8c65e15d6eeb",
     "created_at": "2017-12-27T07:55:22Z",
     "storage_limit": 1,
     "replicas_limit": 2,
@@ -78,8 +75,7 @@ var fakeVolumeData = `
     "price": 0
   },
   {
-    "id": "f853e3f9-1752-42a7-ab07-0ef82cd8e918",
-    "tariff_id": "11a35f90-c343-4fc1-a966-381f75568036",
+    "id": "11a35f90-c343-4fc1-a966-381f75568036",
     "created_at": "2017-12-27T07:55:22Z",
     "storage_limit": 2,
     "replicas_limit": 2,
