27354955607cf4f127554428a17313ee86f11907
Vladimir Stolyarov <xakep6666@gmail.com>
2018-03-20 10:29:21 +0300

    Update vendors

--- vendor/git.containerum.net/ch/kube-client/pkg/cherry/resource-service/rserrors.go
+++ vendor/git.containerum.net/ch/kube-client/pkg/cherry/resource-service/rserrors.go
@@ -176,6 +176,18 @@ func ErrInsufficientStorage(params ...func(*cherry.Err)) *cherry.Err {
 	}
 	return err
 }
+
+func ErrPortsExhausted(params ...func(*cherry.Err)) *cherry.Err {
+	err := &cherry.Err{Message: "Free ports pool for domain exhausted", StatusHTTP: 507, ID: cherry.ErrID{SID: 0x3, Kind: 0xf}, Details: []string(nil)}
+	for _, param := range params {
+		param(err)
+	}
+	for i, detail := range err.Details {
+		det := renderTemplate(detail)
+		err.Details[i] = det
+	}
+	return err
+}
 func renderTemplate(templText string) string {
 	buf := &bytes.Buffer{}
 	templ, err := template.New("").Parse(templText)
