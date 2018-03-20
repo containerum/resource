55d578b887a4c4c239d4a77d67d4808115437d1b
Vladimir Stolyarov <xakep6666@gmail.com>
2018-03-20 10:44:08 +0300

    Make "random_free_domain_port" stored procedure better. Select domain with minimum of used ports. Handle error when free ports exhausted.

---
+++ migrations/1521529941_better_random_free_domain_port.up.sql
@@ -0,0 +1,16 @@
+CREATE OR REPLACE FUNCTION random_free_domain_port(_domain TEXT, _lower INTEGER, _upper INTEGER, _protocol PROTOCOL) RETURNS INTEGER AS $random_free_domain_port$
+DECLARE
+  rand_port INTEGER;
+BEGIN
+  SELECT generate_series(_lower, _upper) AS ser
+  EXCEPT
+  SELECT sp.port
+  FROM service_ports sp
+  JOIN domains d ON sp.domain_id = d.id
+  WHERE (d.domain, sp.protocol) = (_domain, _protocol)
+  ORDER BY ser ASC
+  LIMIT 1
+  INTO rand_port;
+  RETURN rand_port;
+END;
+$random_free_domain_port$ LANGUAGE plpgsql;
