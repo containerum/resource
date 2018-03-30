a07c2ce744bba4fbd87c89f9a99976e9c8bfb914
Vladimir Stolyarov <xakep6666@gmail.com>
2018-03-30 14:56:04 +0300

    Make "namespace_marked_deleted" marks deployments as deleted

---
+++ migrations/1522410427_namespace_marked_deleted_delete_service_deployments.down.sql
@@ -0,0 +1,8 @@
+CREATE OR REPLACE FUNCTION namespace_marked_deleted() RETURNS TRIGGER AS $namespace_marked_deleted$
+BEGIN
+  IF NEW.deleted = TRUE THEN
+    DELETE FROM permissions WHERE resource_id = OLD.id;
+  END IF;
+  RETURN NEW;
+END;
+$namespace_marked_deleted$ LANGUAGE plpgsql;
