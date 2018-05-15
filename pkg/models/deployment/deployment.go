0f1e7a0a4ab34105a98cb86b1ebd1033266ae3f3
paul <petrukhin.paul@gmail.com>
2018-05-15 11:05:51 +0300

    add help methods

--- pkg/models/deployment/deployment.go
+++ pkg/models/deployment/deployment.go
@@ -22,4 +22,38 @@ func DeploymentFromKube(nsID, owner string, deployment model.Deployment) Deploym
 	}
 }
 
-type DeploymentList Deployment
+type DeploymentList []Deployment
+
+func (list DeploymentList) Copy() DeploymentList {
+	return append(make(DeploymentList, 0, list.Len()), list...)
+}
+
+func (list DeploymentList) Len() int {
+	return len(list)
+}
+
+func (list DeploymentList) Names() []string {
+	var names = make([]string, 0, len(list))
+	for _, depl := range list {
+		names = append(names, depl.Name)
+	}
+	return names
+}
+
+func (list DeploymentList) IDs() []string {
+	var IDs = make([]string, 0, len(list))
+	for _, depl := range list {
+		IDs = append(IDs, depl.ID)
+	}
+	return IDs
+}
+
+func (list DeploymentList) Filter(pred func(deployment Deployment) bool) DeploymentList {
+	var filtered = make(DeploymentList, 0, list.Len())
+	for _, depl := range list {
+		if pred(depl) {
+			filtered = append(filtered, depl)
+		}
+	}
+	return filtered
+}
