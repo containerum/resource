6ee8113985fbe7e9b0b52353b76630c793ec9878
paul <petrukhin.paul@gmail.com>
2018-05-15 13:07:06 +0300

    add collection methods

--- pkg/models/service/service.go
+++ pkg/models/service/service.go
@@ -23,6 +23,13 @@ func ServiceFromKube(nsID, owner string, service model.Service) Service {
 	}
 }
 
+func (serv Service) Copy() Service {
+	var cp = serv
+	cp.IPs = append(make([]string, 0, len(cp.IPs)), cp.IPs...)
+	cp.Ports = append(make([]model.ServicePort, 0, len(cp.Ports)), cp.Ports...)
+	return cp
+}
+
 func (serv Service) SelectQuery() interface{} {
 	return bson.M{
 		"namespaceid":  serv.NamespaceID,
@@ -32,5 +39,51 @@ func (serv Service) SelectQuery() interface{} {
 }
 
 func (serv Service) UpdateQuery() interface{} {
-	return serv.Service
+	return bson.M{
+		"$set": bson.M{
+			"service": serv.Service,
+		},
+	}
+}
+
+type ServiceList []Service
+
+func (list ServiceList) Len() int {
+	return len(list)
+}
+
+func (list ServiceList) Names() []string {
+	var names = make([]string, 0, len(list))
+	for _, serv := range list {
+		names = append(names, serv.Name)
+	}
+	return names
+}
+
+func (list ServiceList) Domains() []string {
+	var domains = make([]string, 0, len(list))
+	for _, serv := range list {
+		if serv.Domain != "" {
+			domains = append(domains, serv.Domain)
+		}
+	}
+	return domains
+}
+
+func (list ServiceList) Copy() ServiceList {
+	var cp = make(ServiceList, 0, list.Len())
+	for _, serv := range list {
+		cp = append(cp, serv.Copy())
+	}
+	return cp
+}
+
+func (list ServiceList) Filter(pred func(Service) bool) ServiceList {
+	var filtered = make(ServiceList, 0, list.Len())
+	for _, serv := range list {
+		if pred(serv.Copy()) {
+			filtered = append(filtered, serv.Copy())
+		}
+	}
+	return filtered
 }
