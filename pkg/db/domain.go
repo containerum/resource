108f5cd06d995ae6a86ed5663238ced0a1ccb3c7
paul <petrukhin.paul@gmail.com>
2018-05-16 13:28:57 +0300

    add pagination

--- pkg/db/domain.go
+++ pkg/db/domain.go
@@ -6,12 +6,13 @@ import (
 	"github.com/satori/go.uuid"
 )
 
-func (mongo *MongoStorage) GetDomain(domainName string) (*domain.Domain, error) {
+func (mongo *MongoStorage) GetDomain(domainName string, pages ...uint) (*domain.Domain, error) {
 	mongo.logger.Debugf("getting domain")
 	var collection = mongo.db.C(CollectionDomain)
-	colQuerier := bson.M{"domain": domainName}
-	result := domain.Domain{}
-	if err := collection.Find(colQuerier).One(&result); err != nil {
+	var colQuerier = bson.M{"domain": domainName}
+	var result = domain.Domain{}
+	var query = collection.Find(colQuerier)
+	if err := query.One(&result); err != nil {
 		mongo.logger.WithError(err).Errorf("unable to get domain")
 		return nil, err
 	}
@@ -30,11 +31,29 @@ func (mongo *MongoStorage) GetRandomDomain() (*domain.Domain, error) {
 	return &result, nil
 }
 
-func (mongo *MongoStorage) GetDomainsList() ([]domain.Domain, error) {
+// GetDomainsList supports pagination
+// GetDomainsList(2) will return 2 domains
+// GGetDomainsList(2, 5) will return 2 domains with offset 5
+func (mongo *MongoStorage) GetDomainsList(pages ...int) ([]domain.Domain, error) {
 	mongo.logger.Debugf("getting domain list")
 	var collection = mongo.db.C(CollectionDomain)
 	var result []domain.Domain
-	if err := collection.Find(nil).All(&result); err != nil {
+	var err error
+	var query = collection.Find(nil)
+	switch len(pages) {
+	case 0:
+		err = query.All(&result)
+	case 1:
+		var max = pages[0]
+		err = query.Limit(max).All(&result)
+	case 2:
+		var offset = pages[1]
+		var max = pages[0]
+		err = query.Skip(offset).Limit(max).All(&result)
+	default:
+		mongo.logger.Fatalf("[resource-service/pkg/db.GetDomainList] invalid pagination config: expected at most 2 args, got %d", len(pages))
+	}
+	if err != nil {
 		mongo.logger.WithError(err).Errorf("unable to get domain list")
 		return nil, err
 	}
