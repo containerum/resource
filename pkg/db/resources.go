92d0dd15778a5e75c2ec0896cda90a3ee1dfd2e5
paul <petrukhin.paul@gmail.com>
2018-05-16 16:43:58 +0300

    add method "Get USer Resources"

---
+++ pkg/db/resources.go
@@ -0,0 +1,27 @@
+package db
+
+import (
+	"github.com/containerum/kube-client/pkg/model"
+	"github.com/globalsign/mgo/bson"
+)
+
+func (mongo *MongoStorage) GetUserResources(owner string) (model.Resource, error) {
+	var deployments = mongo.db.C(CollectionDeployment)
+	var res model.Resource
+	return res, deployments.Pipe([]bson.M{
+		{"$project": bson.M{
+			"replicas": "$deployment.replicas",
+			"cpu":      bson.M{"$sum": "$deployment.containers.cpu"},
+			"memory":   bson.M{"$sum": "$deployment.containers.cpu"},
+		}},
+		{"$project": bson.M{
+			"cpu":    bson.M{"$multiply": []string{"$cpu", "$replicas"}},
+			"memory": bson.M{"$multiply": []string{"memory", "$replicas"}},
+		}},
+		{"$group": bson.M{
+			"_id":    256,
+			"cpu":    bson.M{"$sum": "cpu"},
+			"memory": bson.M{"$sum": "memory"},
+		}},
+	}).One(&res)
+}
